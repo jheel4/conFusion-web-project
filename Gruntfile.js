25717517c7249f32960aad6d371fc2f64d9f3e73
Jheel Patel <jheelpatel97@gmail.com>
2017-12-11 23:37:07 +0530

    Final commit

---
+++ Gruntfile.js
@@ -0,0 +1,119 @@
+'use strict';
+
+module.exports=function(grunt){
+	require('time-grunt')(grunt);
+	require('jit-grunt')(grunt);
+	grunt.initConfig({
+		sass:{
+			dist:{
+				files:{
+					'css/styles.css':'css/styles.scss'
+				}
+			}
+		},
+		watch:{
+			files: 'css/*.scss',
+			tasks:['sass']
+		},
+		browserSync:{
+			dev: {
+				bsFiles:{
+					src:[
+						'css/*.css',
+						'.*html',
+						'js/*.js'
+					]
+				},
+				options:{
+					watchTask:true,
+					server:{
+						baseDir:'./'
+					}
+				}
+			}
+		},
+		copy:{
+			html:{
+				files:[{
+					expand:true,
+					dot:true,
+					cwd:'./',
+					src:['*.html'],
+					dest:'dist'
+				}]
+			}
+			fonts:{
+				files:[{
+					expand:true,
+					dot:true,
+					cwd:'node_modules/font-awesome',
+					src:['fonts/*.*'],
+					dest:'dist'
+				}]
+
+			}
+		},
+		clean:{
+			build:{
+				src:['dist/']
+			}
+		},
+
+		imagemin:{
+			dynamic:{
+				files:{
+					expand:true,
+					cwd:'./',
+					src:'img/*.{png,gif,jpg}',
+					dest:'dist'
+				}
+			}
+		},
+		useminPrepare:{
+			foo:{
+				dest:'dist'
+				src:['contactus.html','aboutus.html','index.html'],
+			},
+			options:{
+				flow:{
+					steps:{
+						css:['cssmin'],
+						js:['uglify']
+					}
+					post:{
+						css:[{
+							name:'cssmin',
+							createConfig: function(context,block){
+								var generated=context.options.generated;
+								generated.options={
+									keepSpecialComments:0;
+									rebase:false
+								}
+							}
+						}]
+					}
+				}
+			}
+		},
+		concat:{
+			options:{
+				seperator:';'
+			},
+			dist:{}
+		},
+		uglify:{
+			dist:{}
+		},
+		cssmin:{
+			dist:{}
+		},
+		filerev:{
+			
+		}
+
+	});
+
+	grunt.registerTask('css',['sass']);
+	grunt.registerTask('default',['browserSync','watch']);
+	grunt.registerTask('build',['clean','copy','imagemin']);
+}
